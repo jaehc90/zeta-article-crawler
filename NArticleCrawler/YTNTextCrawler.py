692e02f54d3779cce15ba40b344225176e23b2f9
Kyle <magicsgram@outlook.com>
2017-11-17 17:17:10 +0900

    change file extension naming convention

--- NArticleCrawler/YTNTextCrawler.py
+++ NArticleCrawler/YTNTextCrawler.py
@@ -112,6 +112,7 @@ def dump_ytn_original_scripts(dirname):
             text = f.read()
             f.close()
 
+            postfix = ''
             queryURL = find_url_from_youtube_desc(text)
             if queryURL != "":
                 _text = find_text_ytn(queryURL)
@@ -119,10 +120,10 @@ def dump_ytn_original_scripts(dirname):
                 no_url_counts += 1
                 _text = "No URL Found:\n\n\n" + text
                 print(_text)
-                prefix += ".incomplete"
+                postfix += ".incomplete"
 
-            f = open(os.path.join(dirname, prefix + '.txt'), "w")
-            print("saving {} ...".format(prefix + '.txt'))
+            f = open(os.path.join(dirname, prefix + '.txt' + postfix), "w")
+            print("saving {} ...".format(prefix + '.txt' + postfix))
             f.write(_text)
             f.close()
 
@@ -132,7 +133,7 @@ def get_completescript_from_incomplete(dirname):
     files = os.listdir(dirname)
     no_url_counts = 0
     for i, file in enumerate(files):
-        if '.incomplete.' in file:
+        if '.incomplete' in file:
             prefix = file.split('.')[0]
             queryURL = get_url_from_hash(prefix)
             if queryURL != "":
